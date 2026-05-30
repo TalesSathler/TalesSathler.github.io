60b0efb3e06ae6eeecf9d3cd9034b4e906668703
Tales Sathler Novais <p.talessathler@gmail.com>
2026-05-29 21:03:44 -0300

    refactor: execute serve after start container

--- docker/node/start.sh
+++ docker/node/start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 sudo chmod 775 -R ../
+
 echo 'Container started. Ready for development.'
-echo 'Run: docker exec profile-web bash'
-echo 'Then: npx nx serve --host 0.0.0.0 (for dev server)'
-echo 'Or: npx nx build (for production build)'
+echo 'Run: yarn build (for production build)'
+
+yarn start
 tail -f /dev/null
